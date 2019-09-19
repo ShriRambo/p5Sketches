49a33bbfa6fe474646336560d8748b18031b9443
Shriram <sashirgade+github@gmail.com>
2019-09-19 16:03:56 +0200

    Water color blob init
    Initialized water color blob sketch

--- Water color blob/sketch.js
+++ Water color blob/sketch.js
@@ -6,9 +6,9 @@
 //   |::.. . |                                             
 //   `-------'      
 
-
 var Canvass;
 
+var hexagon = [ ]
 
 function preload() {
 
@@ -17,8 +17,15 @@ function preload() {
 function setup() {
     Canvass = createCanvas(400,420);
     background(51);
+
+    ellipse(0.5*width,0.5*height,10)
+    let vec = createVector(1,0).setMag(120);
+    for (let i = 0; i < 6; i++){
+        hexagon.push({x:vec.x + width/2,y:vec.y+height/2})
+        vec.rotate(PI/3);
+    }
+    drawTheShape(hexagon);
     noLoop();
-    
 }
 
 function draw() {
@@ -34,5 +41,24 @@ function keyPressed() {
     }
 }
 
+function drawTheShape(theShape){
 
+    push()
+    stroke(200);
+    noFill();
+    strokeWeight(3);
+    beginShape()
+        for (let pt of theShape){
+            vertex(pt.x,pt.y)
+            ellipse(pt.x, pt.y, 10)
+        }
+    endShape(CLOSE)
+
+    pop()
+}
+
+
+function getNewShape(){
+
+}
 

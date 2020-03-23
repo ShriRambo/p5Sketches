d2f762e62e22e51d5e0eed67620ea3142de89a58
shrirambo <sashirgade@gmail.com>
2020-03-23 16:30:52 +0100

    Rorschach_v2

--- Metaballs/Rorschach - Colored/sketch.js
+++ Metaballs/Rorschach - Colored/sketch.js
@@ -16,6 +16,7 @@ var metaR
 function setup() {
     Canvass = createCanvas(400,500);
     background(200);
+    
     metaR = new MetaBallRender()
     setupRenderer()
     // noLoop()
@@ -28,17 +29,15 @@ function draw() {
     // background(200,20)
     metaR.show()
     metaR.update()
-    if(frameCount % 40 == 0) console.log("sec")
+    if(frameCount % 30 == 0) console.log("sec")
     
     // noLoop();
     // console.log(frameRate())
 }
 
 function setupRenderer() {
-    capturer = new CCapture({ format: 'webm', framerate: 40 });
+    capturer = new CCapture({ format: 'webm', framerate: 30 });
     capturer.start();
-    
-
     rendering = true
 }
 
@@ -47,6 +46,7 @@ function keyPressed() {
     if(keyCode == DELETE){
         console.log('Animation Stopped');
         noLoop();
+        save(Canvass, "sketch.jpg")
     }
     if ((keyCode == ESCAPE) & rendering) {
 
@@ -66,11 +66,13 @@ class Ball{
         this.mag = random()*(0.5*random() + 0.1)*width
         this.dir = random([1,-1])
         this.dtheta = random()*0.1
-        this.r = 1 + 10*random()
+        this.r = 4 + 10*random()
         this.dy = 100*(2*random() - 1)
         this.x = 0
         this.y = 0
-        this.frr = 0.01
+        this.frr = 0.03
+
+        this.mult = random([1,1,1,-1])
         // this.col = random( ["#6CC2BD", "#5A819E", "#5A819E", "#F67E7D", "#FABFA6",  "#FFEAD0" ])
 
         this.pseed = 10000*random()
@@ -106,7 +108,7 @@ class Ball{
 class MetaBallRender{
     constructor(){
 
-        this.nballs = 20
+        this.nballs = 40
         this.balls = []
         for(let i = 0; i < this.nballs; i++){
             this.balls[i] = new Ball()
@@ -115,6 +117,8 @@ class MetaBallRender{
         this.mul = 4;
         this.bgcol = "#E3E3E3" //"#F3ECDD"
         this.ink = "#282828"
+
+        this.cols = ["#E3E3E3", "#282828" ]
 
         this.cpickFn =  chroma.scale([this.bgcol, this.ink]);
 
@@ -142,18 +146,22 @@ class MetaBallRender{
 
         // Original balls
         for(let ball of this.balls){
-            va += min(1, ball.r/dist(x,y,ball.x,ball.y))
-            va += min(1,ball.r/ dist(x,y, width-ball.x, ball.y))
+            va += (ball.mult*( ball.r/dist(x,y,ball.x,ball.y)) )
+            va += ( ball.mult*(ball.r/ dist(x,y, width-ball.x, ball.y)))
         }
 
         va = this.mul*va/(2*this.nballs)
         // console.log(va)
 
         let p1 = min(x,  (width - x))
-        if(va < 0.1 + 0.15*noise(frameCount*0.001, p1*0.05, y*0.05) ) va = 0 
+        if(va < 0.1 + 0.1*noise(frameCount*0.001, p1*0.1, y*0.1) ) va = 0 
         else va = 1
-        let co = this.cpickFn(va)
-        return color(co.hex())
+
+        // Use cpickFn for continuous spectrum
+        // let co = this.cpickFn(va)
+        // return color(co.hex())
+
+        return color(this.cols[va])
 
     }
 
